e5e5cdfd509bb9b086a97b47d918f0d9b0076e76
GitanElyon <gitanelyon@gmail.com>
2026-02-07 16:42:47 -0500

    added flake and changed licence to MIT

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A simple Blackjack-AI for testing and learning different strategies.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk' = pkgs.callPackage naersk {};
+      in
+        {
+          packages.default = naersk'.buildPackage {
+            pname = "blackjack-ai";
+            src = ./.;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              rust-analyzer
+              pkg-config
+            ];
+          };
+        }
+    );
+}
